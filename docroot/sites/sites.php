25ce041affd60dbabd458a4f312f8ee6b1d24449
Matthew Brabham <brabham.matthew@gmail.com>
2021-12-21 16:41:23 -0800

    Removing sites no longer needed

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,8 +1,6 @@
 <?php
 $sites['brabhamm.dev.oregonstate.edu'] = 'brabhamm.cws.oregonstate.edu';
 $sites['brabhamm.stage.oregonstate.edu'] = 'brabhamm.cws.oregonstate.edu';
-$sites['dayo.dev.oregonstate.edu'] = 'dayo.cws.oregonstate.edu';
-$sites['dayo.stage.oregonstate.edu'] = 'dayo.cws.oregonstate.edu';
 $sites['drupal-next.dev.oregonstate.edu'] = 'drupal-next.cws.oregonstate.edu';
 $sites['drupal-next.stage.oregonstate.edu'] = 'drupal-next.cws.oregonstate.edu';
 $sites['fenns.dev.oregonstate.edu'] = 'fenns.cws.oregonstate.edu';
@@ -11,10 +9,6 @@ $sites['hansenm.dev.oregonstate.edu'] = 'hansenm.cws.oregonstate.edu';
 $sites['hansenm.stage.oregonstate.edu'] = 'hansenm.cws.oregonstate.edu';
 $sites['holcombk.dev.oregonstate.edu'] = 'holcombk.cws.oregonstate.edu';
 $sites['holcombk.stage.oregonstate.edu'] = 'holcombk.cws.oregonstate.edu';
-$sites['mcdonalm.dev.oregonstate.edu'] = 'mcdonalm.cws.oregonstate.edu';
-$sites['mcdonalm.stage.oregonstate.edu'] = 'mcdonalm.cws.oregonstate.edu';
-$sites['paynesar.dev.oregonstate.edu'] = 'paynesar.cws.oregonstate.edu';
-$sites['paynesar.stage.oregonstate.edu'] = 'paynesar.cws.oregonstate.edu';
 $sites['profiles.dev.oregonstate.edu'] = 'profiles.oregonstate.edu';
 $sites['profiles.stage.oregonstate.edu'] = 'profiles.oregonstate.edu';
 $sites['simsk.dev.oregonstate.edu'] = 'simsk.cws.oregonstate.edu';
