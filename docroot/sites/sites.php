2cb07fc05b4e11e30eee4124035b0f226c4b2088
Matthew Brabham <brabham.matthew@gmail.com>
2022-03-04 14:11:00 -0800

    Adding new dev sites

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,17 +1,23 @@
 <?php
 $sites['brabhamm.dev.oregonstate.edu'] = 'brabhamm.cws.oregonstate.edu';
 $sites['brabhamm.stage.oregonstate.edu'] = 'brabhamm.cws.oregonstate.edu';
+$sites['calverta.dev.oregonstate.edu'] = 'calverta.cws.oregonstate.edu';
+$sites['calverta.stage.oregonstate.edu'] = 'calverta.cws.oregonstate.edu';
 $sites['drupal-next.dev.oregonstate.edu'] = 'drupal-next.cws.oregonstate.edu';
 $sites['drupal-next.stage.oregonstate.edu'] = 'drupal-next.cws.oregonstate.edu';
 $sites['engr.dev.oregonstate.edu'] = 'engr.cws.oregonstate.edu';
 $sites['engr.stage.oregonstate.edu'] = 'engr.cws.oregonstate.edu';
+$sites['extension.dev.oregonstate.edu'] = 'extension.cws.oregonstate.edu';
+$sites['extension.stage.oregonstate.edu'] = 'extension.cws.oregonstate.edu';
 $sites['fenns.dev.oregonstate.edu'] = 'fenns.cws.oregonstate.edu';
 $sites['fenns.stage.oregonstate.edu'] = 'fenns.cws.oregonstate.edu';
 $sites['hansenm.dev.oregonstate.edu'] = 'hansenm.cws.oregonstate.edu';
 $sites['hansenm.stage.oregonstate.edu'] = 'hansenm.cws.oregonstate.edu';
 $sites['holcombk.dev.oregonstate.edu'] = 'holcombk.cws.oregonstate.edu';
 $sites['holcombk.stage.oregonstate.edu'] = 'holcombk.cws.oregonstate.edu';
+$sites['leighr.dev.oregonstate.edu'] = 'leighr.cws.oregonstate.edu';
+$sites['leighr.stage.oregonstate.edu'] = 'leighr.cws.oregonstate.edu';
+$sites['mcqueenj.dev.oregonstate.edu'] = 'mcqueenj.cws.oregonstate.edu';
+$sites['mcqueenj.stage.oregonstate.edu'] = 'mcqueenj.cws.oregonstate.edu';
 $sites['profiles.dev.oregonstate.edu'] = 'profiles.oregonstate.edu';
 $sites['profiles.stage.oregonstate.edu'] = 'profiles.oregonstate.edu';
-$sites['simsk.dev.oregonstate.edu'] = 'simsk.cws.oregonstate.edu';
-$sites['simsk.stage.oregonstate.edu'] = 'simsk.cws.oregonstate.edu';
