2f4982188542640f98783b434135c0b3d832db3d
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-05-13 10:14:53 -0700

    Update Readme for steps needed to create new sites. Update generator script to give post install steps until we connect it to acquia.

--- scripts/composer/SiteGenerator.php
+++ scripts/composer/SiteGenerator.php
@@ -15,28 +15,38 @@ class SiteGenerator {
     $io = $event->getIO();
     $baseDir = getcwd();
     $siteName = strtolower($io->ask('Site Production FQDN: '));
-    if (!is_dir($baseDir . "/docroot/sites/$siteName")) {
+    if (!is_dir($baseDir . "/docroot/sites/{$siteName}")) {
       $databaseName = str_replace('.', '_', $siteName);
-      mkdir($baseDir . "/docroot/sites/$siteName");
+      mkdir($baseDir . "/docroot/sites/{$siteName}");
       $settingsFile = file_get_contents($baseDir . "/scripts/site-template/settings.php");
       $settingsFile = str_replace("DOMAIN", $databaseName, $settingsFile);
-      file_put_contents($baseDir . "/docroot/sites/$siteName/settings.php", $settingsFile);
-      copy($baseDir . "/scripts/site-template/cloud-memcache-d8+.php", $baseDir . "/docroot/sites/$siteName/cloud-memcache-d8+.php");
+      file_put_contents($baseDir . "/docroot/sites/{$siteName}/settings.php", $settingsFile);
+      copy($baseDir . "/scripts/site-template/cloud-memcache-d8+.php", $baseDir . "/docroot/sites/{$siteName}/cloud-memcache-d8+.php");
       // Update sites.php file with new domain
       /* @var $sites */
       include($baseDir . '/docroot/sites/sites.php');
+      // Check to ensure domain doesn't already exist in sites.php.
       if (!in_array($siteName, $sites)) {
         $siteSubName = explode('.', $siteName);
-        $sites[$siteSubName[0] . '.dev.oregonstate.edu'] = $siteName;
-        $sites[$siteSubName[0] . '.stage.oregonstate.edu'] = $siteName;
+        $sites["{$siteSubName[0]}.dev.oregonstate.edu"] = $siteName;
+        $sites["{$siteSubName[0]}.stage.oregonstate.edu"] = $siteName;
       }
       ksort($sites);
       $string = '<?php' . PHP_EOL;
       file_put_contents($baseDir . '/docroot/sites/sites.php', $string);
       foreach ($sites as $key => $value) {
-        $siteString = "\$sites['$key'] = '$value';" . PHP_EOL;
+        $siteString = "\$sites['{$key}'] = '{$value}';" . PHP_EOL;
         file_put_contents($baseDir . '/docroot/sites/sites.php', $siteString, FILE_APPEND);
       }
+      $postInstallSteps = [
+        "Post Script Steps:",
+        "Log into Acquia Cloud and Create the following Database and Domains:",
+        "Database: {$databaseName}",
+        "Dev Domain: {$siteSubName[0]}.dev.oregonstate.edu",
+        "Test Domain: {$siteSubName[0]}.stage.oregonstate.edu",
+        "Prod Domain: {$siteName}",
+      ];
+      $io->write($postInstallSteps);
     }
     else {
       $io->writeError('Site Already exists');
