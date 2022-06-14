3eee4a8fe36921c720f8e495d987970ace147859
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-06-13 17:00:41 -0700

    Removed cloud hook that doesn't work currently

--- hooks/prod/post-db-copy/02-warm-caches.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-site="$1"
-target_env="$2"
-echo"Using Drush Version"
-echo $(drush9 --version --no-ansi)
-drush9 @$site.$target_env warmer:enqueue jsonapi --no-ansi
-drush9 @$site.$target_env queue:run warmer --no-ansi
