70f8152c29e553a060739c7e448c3c8449cd8a3f
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-06-24 16:52:11 -0700

    Removing comment line no longer needed

--- hooks/common/post-code-update/01-update-db.sh
+++ hooks/common/post-code-update/01-update-db.sh
@@ -7,7 +7,6 @@
 
 site="$1"
 target_env="$2"
-#drush10 @$site.$target_env updatedb --yes --no-ansi
 find sites -maxdepth 1 -type d -not -name sites -printf '%f\n' | while IFS=/ read -r DOMAIN; do printf "%s," "${DOMAIN}"; \
   drush @"${site}"."${target_env}" -l "${DOMAIN}" updatedb -y; \
   drush @"${site}"."${target_env}" -l "${DOMAIN}" cr; \
