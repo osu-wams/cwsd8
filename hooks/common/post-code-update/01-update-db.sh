97f2cc499f0a82ad0a3444f8fb912fcc9c596784
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-06-24 16:59:28 -0700

    Update the find path

--- hooks/common/post-code-update/01-update-db.sh
+++ hooks/common/post-code-update/01-update-db.sh
@@ -7,7 +7,9 @@
 
 site="$1"
 target_env="$2"
-find sites -maxdepth 1 -type d -not -name sites -printf '%f\n' | while IFS=/ read -r DOMAIN; do printf "%s," "${DOMAIN}"; \
+app_root="/var/www/html/${site}.${target_env}/docroot"
+sites_folder="${app_root}/sites"
+find ${sites_folder} -maxdepth 1 -type d -not -name sites -printf '%f\n' | while IFS=/ read -r DOMAIN; do printf "%s," "${DOMAIN}"; \
   drush @"${site}"."${target_env}" -l "${DOMAIN}" updatedb -y; \
   drush @"${site}"."${target_env}" -l "${DOMAIN}" cr; \
 done
