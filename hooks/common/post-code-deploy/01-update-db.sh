c0f4789f228828478a3534d73a1d37be23caff26
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-06-23 17:04:47 -0700

    trying hook for updates

--- hooks/common/post-code-deploy/01-update-db.sh
+++ hooks/common/post-code-deploy/01-update-db.sh
@@ -9,4 +9,5 @@ site="$1"
 target_env="$2"
 echo"Using Drush Launcher Version"
 echo $(drush10 --version)
-drush10 @$site.$target_env updatedb --yes --no-ansi
+#drush10 @$site.$target_env updatedb --yes --no-ansi
+find sites -maxdepth 1 -type d -not -name sites -printf '%f\n' | while IFS=/ read -r DOMAIN; do printf "%s," ${DOMAIN}; drush -l $DOMAIN updatedb -y; drush -l $DOMAIN cr; done
