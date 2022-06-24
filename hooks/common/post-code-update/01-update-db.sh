e4bfdc346f098a82b421cf9ab2d253fabe34ec60
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-06-24 16:50:35 -0700

    Update script to best coding practices

--- hooks/common/post-code-update/01-update-db.sh
+++ hooks/common/post-code-update/01-update-db.sh
@@ -7,6 +7,8 @@
 
 site="$1"
 target_env="$2"
-echo"Using Drush Launcher Version"
-echo $(drush10 --version)
-drush10 @$site.$target_env updatedb --yes --no-ansi
+#drush10 @$site.$target_env updatedb --yes --no-ansi
+find sites -maxdepth 1 -type d -not -name sites -printf '%f\n' | while IFS=/ read -r DOMAIN; do printf "%s," "${DOMAIN}"; \
+  drush @"${site}"."${target_env}" -l "${DOMAIN}" updatedb -y; \
+  drush @"${site}"."${target_env}" -l "${DOMAIN}" cr; \
+done
